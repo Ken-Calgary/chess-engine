d01f7065e14ebabeab3c47c170e68da4536207f3
Soul <ken.liu1@ucalgary.ca>
2023-10-05 19:09:00 -0600

    Updated Board.java
    - Changed adding pawns into a for loop for cleaner code

--- main/Board.java
+++ main/Board.java
@@ -14,21 +14,19 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Board extends JPanel {
-    public int tileSize = 85;
+    public int tileSize = 100;
 
     final int COLUMNS = 8;
     final int ROWS = 8;
     
     ArrayList<Piece> pieceList = new ArrayList<>();
-
-    // Creates the board when called in Main
     public Board() {
         this.setPreferredSize(new Dimension(COLUMNS*tileSize, ROWS*tileSize));
         addBlackPieces();
         addWhitePieces();
     }
 
-    // Adds the all  black pieces onto the board
+    // Adds the all black pieces onto the board
     public void addBlackPieces() {
     	pieceList.add(new King(this, 4, 0, false));
     	pieceList.add(new Queen(this, 3, 0, false));
@@ -39,17 +37,12 @@ public class Board extends JPanel {
     	pieceList.add(new Rook(this, 0, 0, false));
     	pieceList.add(new Rook(this, 7, 0, false));
     	
-    	pieceList.add(new Pawn(this, 0, 1, false));
-    	pieceList.add(new Pawn(this, 1, 1, false));
-    	pieceList.add(new Pawn(this, 2, 1, false));
-    	pieceList.add(new Pawn(this, 3, 1, false));
-    	pieceList.add(new Pawn(this, 4, 1, false));
-    	pieceList.add(new Pawn(this, 5, 1, false));
-    	pieceList.add(new Pawn(this, 6, 1, false));
-    	pieceList.add(new Pawn(this, 7, 1, false));
+    	for (int i = 0; i < COLUMNS; i++) {
+        	pieceList.add(new Pawn(this, i, 1, false));
+    	}
     }
     
-    // Add all white pieces onto the board
+	// Adds the all white pieces onto the board
     public void addWhitePieces() {
     	pieceList.add(new King(this, 4, 7, true));
     	pieceList.add(new Queen(this, 3, 7, true));
@@ -60,14 +53,9 @@ public class Board extends JPanel {
     	pieceList.add(new Rook(this, 0, 7, true));
     	pieceList.add(new Rook(this, 7, 7, true));
     	
-    	pieceList.add(new Pawn(this, 0, 6, true));
-    	pieceList.add(new Pawn(this, 1, 6, true));
-    	pieceList.add(new Pawn(this, 2, 6, true));
-    	pieceList.add(new Pawn(this, 3, 6, true));
-    	pieceList.add(new Pawn(this, 4, 6, true));
-    	pieceList.add(new Pawn(this, 5, 6, true));
-    	pieceList.add(new Pawn(this, 6, 6, true));
-    	pieceList.add(new Pawn(this, 7, 6, true));
+    	for (int i = 0; i < COLUMNS; i++) {
+        	pieceList.add(new Pawn(this, i, 6, true));
+    	}
     }
     
     
@@ -75,7 +63,6 @@ public class Board extends JPanel {
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
-        // Paints the 8x8 chess board
         for (int r = 0; r < ROWS; r++) {
             for (int c = 0 ; c < COLUMNS; c++) {
                 g2d.setColor((c+r) % 2 == 0 ? new Color(227,198,181): new Color(157,105,53));
